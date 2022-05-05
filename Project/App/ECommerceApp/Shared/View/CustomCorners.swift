75f9917552cffb4997737a0500e578599519e07f
studyLiyuhong165 <study_liyuhong165@163.com>
2022-05-05 13:34:27 +0800

    no message

--- Project/App/ECommerceApp/Shared/View/CustomCorners.swift
+++ Project/App/ECommerceApp/Shared/View/CustomCorners.swift
@@ -16,6 +16,9 @@ struct CustomCorners: Shape {
     
     func path(in rect: CGRect) -> Path {
         //  通过贝塞尔画一个形状
+        // 贝塞尔曲线 可以参考
+        // https://baike.baidu.com/item/%E8%B4%9D%E5%A1%9E%E5%B0%94%E6%9B%B2%E7%BA%BF/1091769
+        // http://www.demodashi.com/demo/11981.html
         let path = UIBezierPath(roundedRect: rect,
                                 byRoundingCorners: corners,
                                 cornerRadii: CGSize(width: radius, height: radius))
